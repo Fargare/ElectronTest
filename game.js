f1274012d7031fce4f34fcdf719be9829e5b049e
Fargare <gleaming7moonlight29@gmail.com>
2017-06-30 17:56:35 +0900

    宝箱の画像およびアニメーションの追加

--- game.js
+++ game.js
@@ -1,47 +1,12 @@
 const SCREEN_WIDTH = 800;
 const SCREEN_HEIGHT=600;
 
-const five = require("johnny-five");
-const board = new five.Board();
-const button;
-
-board.on("ready", function() {
-  console.log("ready");
-  // スイッチの設定
-  button = new five.Button({
-    // デジタル2番ピンにスイッチを接続
-    pin: 6,
-    // Arduinoに内蔵されているプルアップ回路を有効
-    isPullup: true
-  });
-
-  // スイッチを追加(アクセス許可)
-  board.repl.inject({
-    button: button
-  });
-
-  // スイッチを押した
-  button.on("down", function() {
-    console.log("HIGH");
-  });
-
-  // スイッチを押し続けて一定時間(初期設定では500ms)経過した
-  button.on("hold", function() {
-    console.log("HOLD");
-  });
-
-  // スイッチを離した
-  button.on("up", function() {
-    console.log("LOW");
-  });
-});
-
-
-
 const ASSETS = {
   image: {
-    bg: "http://www.southernfriedscience.com/wp-content/uploads/2015/02/bioshock-rapture-city1.jpg",
-    chest: "http://clipartwork.com/wp-content/uploads/2017/02/of-a-treasure-chest-free-download.png",
+    bg: "Resources/BG.jpg",
+    chest: "Resources/宝箱.png",
+    chest_empty: "Resources/宝箱空.png",
+    chest_gold: "Resources/宝箱金貨.png",
   },
 };
 
@@ -58,14 +23,81 @@ phina.define('MainScene', {
     });
     // 背景色を指定
     this.backgroundColor = '#ffffff';
+
+    this.bg = Sprite("bg",SCREEN_WIDTH,SCREEN_HEIGHT).addChildTo(this);
+    this.bg.origin.set(0,0);
+
+    const chest_L = Sprite('chest').addChildTo(this);
+    const chest_R = Sprite('chest').addChildTo(this);
+    chest_L.x= this.gridX.center(-3);
+    chest_R.x= this.gridX.center(3);
+    chest_L.y= chest_R.y = this.gridY.center(2);
+    chest_L.width = chest_R.width = 220;
+    chest_L.height = chest_R.height = 220;
+
+    this.chest_L = chest_L;
+    this.chest_R = chest_R;
+
+    const animation_flag = 0;
+    this.animation_flag = animation_flag;
+    // const chest_empty_R = Sprite('chest_empty').addChildTo(this);
+    //
+    // const chest_empty_L = Sprite('chest_empty').addChildTo(this);
+    // chest_empty.x= this.gridX.center(-3);
+    // chest_empty.y= this.gridY.center(2);
+    // chest_empty.width = 220;
+    // chest_empty.height = 220;
+    // const chest_gold_L = Sprite('chest_gold').addChildTo(this);
+    // const chest_gold_R = Sprite('chest_gold').addChildTo(this);
+    //
+    // chest_gold.x= this.gridX.center(+3);
+    // chest_gold.y= this.gridY.center(2);
+    // chest_gold.width = 220;
+    // chest_gold.height = 220;
+
     // ラベルを生成
-    this.label = Label('Hello, phina.js!').addChildTo(this);
+    this.label = Label('金貨の入った宝箱をあてよう！！').addChildTo(this);
     this.label.x = this.gridX.center(); // x 座標
-    this.label.y = this.gridY.center(); // y 座標
-    this.label.fill = 'black'; // 塗りつぶし色
+    this.label.y = this.gridY.center(-4); // y 座標
+    this.label.fontSize = 40,
+    this.label.fill = 'white'; // 塗りつぶし色
   },
   update: function(app) {
+    // console.log(this.animation_flag);
+    const keyboard = app.keyboard;
+    if(keyboard.getKey('a')){
+      if(this.animation_flag == 0){
+        this.animation_flag = 1;
+      }
+    };
 
+    if(keyboard.getKey('b')){
+      this.chest_R.setImage("chest_gold",220,220);
+    };
+
+    if(this.animation_flag == 1){
+      this.animation_flag = 2;
+      const self = this.chest_L;
+      this.chest_L.tweener
+      .to({
+        scaleX:1.2,
+        scaleY:1.2,
+      },200,'easeOutBounce')
+      .to({
+        scaleX:1,
+        scaleY:1,
+      },200,'easeInBounce')
+      .wait(500)
+      .to({
+        scaleX:1.2,
+        scaleY:1.2,
+      },200,'easeInQuart')
+      .call(function(){
+        console.log("animation done")
+        self.setImage("chest_empty",220,220);
+      }).setLoop(false);
+
+    }
   }
 });
 
@@ -74,6 +106,7 @@ phina.main(function() {
   // アプリケーション生成
   const app = GameApp({
     startLabel: 'main', // メインシーンから開始する
+    assets:ASSETS,
     fit :true,
     width:SCREEN_WIDTH,
     height:SCREEN_HEIGHT,
